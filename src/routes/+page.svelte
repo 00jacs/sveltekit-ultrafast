c2fd97006a240525987dd47658f76b60f1e00d00
gillbatesdev <gillbatesdev@gmail.com>
2024-05-19 11:54:15 +0200

    feat: init project with tailwind and daisyui

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	let counter = 0;
+</script>
+
+<div class="max-w-xl py-6 px-6 my-12 mx-auto border rounded-xl">
+	<p class="mb-4">
+		<span class="font-bold">Counter: </span>
+		{counter}
+	</p>
+	<button class="btn btn-outline" on:click={() => (counter += 1)}>Increment</button>
+</div>
