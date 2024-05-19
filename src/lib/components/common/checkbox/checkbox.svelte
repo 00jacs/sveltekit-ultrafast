43d86b63bc51e52627e7dd33b55db26e6402fa77
gillbatesdev <gillbatesdev@gmail.com>
2024-05-19 16:50:21 +0200

    feat(ui): created a checkbox reusable component

---
+++ src/lib/components/common/checkbox/checkbox.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	export let id: string = '';
+	export let name: string = '';
+	export let checked: boolean = false;
+	export let label: string = '';
+
+	export let containerClass: string = '';
+	export let labelClass: string = '';
+	export let inputClass: string = '';
+
+	const dispatch = createEventDispatcher();
+
+	function handleChange(e: Event) {
+		checked = (e.target as HTMLInputElement).checked;
+		dispatch('change', checked);
+	}
+</script>
+
+<div class="form-control {containerClass}">
+	<label for={id || undefined} class="label cursor-pointer justify-start gap-2">
+		<input
+			type="checkbox"
+			{id}
+			{name}
+			class="checkbox-primary checkbox checkbox-xs {inputClass}"
+			bind:checked
+			on:change={handleChange} />
+
+		<span class="label-text {labelClass}">
+			<slot>{label}</slot>
+		</span>
+	</label>
+</div>
--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,6 +1,7 @@
 import TextInput from './common/text-input/text-input.svelte';
+import Checkbox from './common/checkbox/checkbox.svelte';
 
 import Notifications from './page/notifications/notifications.svelte';
 import { notify } from './page/notifications/notifications-store';
 
-export { TextInput, Notifications, notify };
+export { TextInput, Checkbox, Notifications, notify };
