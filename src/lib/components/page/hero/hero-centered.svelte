68cccd75ab39f604fee585eda8aa668b8ad8c3a6
jacobxcoder <gillbatesdev@gmail.com>
2024-05-30 16:30:27 +0200

    feat(ui): add hero-centered as a landing page component

---
+++ src/lib/components/page/hero/hero-centered.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	const hero = {
+		title: 'sveltekit-ultrafast ⚡️',
+		subtitle: 'A blazing fast SvelteKit template with Tailwind CSS and TypeScript.',
+		note: {
+			text: 'See how it helps you build start-ups',
+			link: 'https://x.com/jacob_the_coder',
+			linkText: 'here'
+		},
+		secondaryButton: {
+			text: 'Follow on Twitter',
+			link: 'https://twitter.com/jacob_the_coder'
+		},
+		ctaButton: {
+			text: 'Get started',
+			link: '/docs'
+		}
+	} as const;
+</script>
+
+<div class="mx-auto max-w-2xl py-16 sm:py-24 lg:py-32">
+	<div class="hidden sm:mb-8 sm:flex sm:justify-center">
+		<div
+			class="text-base-content-secondary relative rounded-full px-3
+                    py-1 text-sm leading-6 ring-1 ring-base-300">
+			{hero.note.text}
+			<a href={hero.note.link} target="_blank" class="font-semibold text-primary">
+				{hero.note.linkText}
+			</a>
+		</div>
+	</div>
+	<div class="text-center">
+		<h1 class="text-4xl font-bold leading-tight tracking-tight sm:text-6xl">
+			{hero.title}
+		</h1>
+		<p class="text-base-content-secondary mt-6 leading-8">
+			{hero.subtitle}
+		</p>
+		<div class="mt-10 flex items-center justify-center gap-x-6">
+			<a
+				href={hero.secondaryButton.link}
+				target="_blank"
+				class="link text-sm font-semibold leading-6 no-underline">
+				{hero.secondaryButton.text}
+			</a>
+
+			<a href={hero.ctaButton.link} class="btn btn-primary font-semibold">
+				{hero.ctaButton.text}
+			</a>
+		</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import HeroCentered from '$lib/components/page/hero/hero-centered.svelte';
+
 	const examplePages = [
 		{
 			name: 'PostgreSQL database with Supabase',
@@ -23,24 +25,9 @@
 	] as const;
 </script>
 
-<section id="hero" class="mx-auto my-12 max-w-2xl px-4 py-12 text-center">
-	<h1 class="mb-3 text-4xl font-bold">
-		Welcome to
-		<span class="text-primary">sveltekit-ultrafast</span> ⚡️
-	</h1>
+<HeroCentered />
 
-	<p class="text-base-content-secondary mb-5">
-		This boilerplate allows you to rapidly prototype a SvelteKit app for your next
-		start-up.
-	</p>
-
-	<div class="flex justify-center gap-2">
-		<a href="#do" class="btn btn-outline border-base-300">Follow on Twitter</a>
-		<a href="/get-started" class="btn btn-primary">Get started</a>
-	</div>
-</section>
-
-<section id="examples" class="mx-auto max-w-2xl text-center">
+<section id="examples" class="mx-auto max-w-2xl pb-16 text-center">
 	<h2 class="mb-3 text-3xl font-bold">Example pages</h2>
 
 	<ul>
