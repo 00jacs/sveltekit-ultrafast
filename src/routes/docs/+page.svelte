1c794c780f1a170a0d1458277a3b34616b02bf3e
jacobxcoder <gillbatesdev@gmail.com>
2024-05-31 15:37:29 +0200

    feat(docs): add proper redirect to get-started

--- src/routes/docs/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import DocsPage from '$lib/components/page/docs/page.svelte';
-</script>
-
-<DocsPage title="Credits" subtitle="The people behind the project are truly incredible.">
-	<p>Here is the content...</p>
-</DocsPage>
