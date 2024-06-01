741960c07345ab53f93081504c2662e7ccb63857
jacobxcoder <gillbatesdev@gmail.com>
2024-06-01 08:33:53 +0200

    feat(docs): add responsive mobile support, add theme toggler

--- src/routes/docs/+layout.svelte
+++ src/routes/docs/+layout.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import { Button } from '$lib/components';
+	import { theme, Theme, toggleTheme } from '$lib/stores/theme';
+	import { Icon, Bars3, XMark, Sun, Moon } from 'svelte-hero-icons';
 
 	interface DocsItem {
 		title: string;
@@ -81,12 +84,37 @@
 			]
 		}
 	];
+
+	let mobileMenuOpen = false;
+	let isLightTheme = $theme === Theme.LIGHT;
+
+	$: if (mobileMenuOpen) {
+		document.body.style.overflow = 'hidden';
+	} else {
+		document.body.style.overflow = 'auto';
+	}
+
+	$: $page.url.pathname.startsWith('/docs') && (mobileMenuOpen = false);
 </script>
 
 <div class="flex h-full">
+	<Button
+		class="btn-ghost fixed left-3 top-3 z-50 bg-base-200 md:hidden"
+		on:click={() => (mobileMenuOpen = !mobileMenuOpen)}>
+		{#if mobileMenuOpen}
+			<Icon src={XMark} class="h-6 w-6" />
+		{:else}
+			<Icon src={Bars3} class="h-6 w-6" />
+		{/if}
+	</Button>
+
 	<nav
 		id="navigation"
-		class="fixed left-0 top-0 h-full w-64 overscroll-auto border-r-2 border-base-200 bg-base-200 bg-opacity-50 py-12 pl-8 pr-12">
+		class="left-0 top-0 z-40 hidden h-full w-64 flex-col justify-between overscroll-auto
+            border-r-2 border-base-200 bg-base-200 bg-opacity-50 py-12 pl-8
+            pr-12 md:fixed md:flex {mobileMenuOpen
+			? '!fixed left-0 top-0 !block h-full w-full !bg-base-200'
+			: ''}">
 		<ul class="text-base-content-secondary">
 			{#each list as item}
 				<li class="relative mb-5 mt-4">
@@ -94,7 +122,8 @@
 						href={item.href}
 						class="link mb-3 block no-underline {$page.url.pathname === item.href
 							? 'text-primary'
-							: 'text-base-content-secondary'}">
+							: 'text-base-content-secondary'}"
+						on:click={() => (mobileMenuOpen = false)}>
 						{item.title}
 					</a>
 
@@ -117,9 +146,20 @@
 				</li>
 			{/each}
 		</ul>
+
+		<label class="swap swap-rotate mt-10">
+			<input
+				type="checkbox"
+				class="hidden"
+				checked={isLightTheme}
+				on:change={toggleTheme} />
+
+			<Icon src={Sun} class="swap-on h-5 w-5 fill-current" />
+			<Icon src={Moon} class="swap-off h-5 w-5 fill-current" />
+		</label>
 	</nav>
 
-	<main id="docs-content" class="absolute left-64 p-8">
+	<main id="docs-content" class="w-[100%] p-8 md:absolute md:left-64 md:w-auto">
 		<slot />
 	</main>
 </div>
