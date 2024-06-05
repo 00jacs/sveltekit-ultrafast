30d11bc3d68a4dfaab7380a38719e9c137a00fd6
jacobxcoder <gillbatesdev@gmail.com>
2024-06-05 22:09:49 +0200

    refactor(utils): get rid of eslint/ts issues

--- src/lib/utils/logger.ts
+++ src/lib/utils/logger.ts
@@ -2,21 +2,19 @@ function getFormattedDate(): string {
 	return new Date().toISOString();
 }
 
+type LogData = unknown[];
+
 export const logger = {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	info(...data: any[]): void {
+	info(...data: LogData): void {
 		console.log(`\x1b[36m%s\x1b[0m`, `[info] [${getFormattedDate()}]: `, ...data);
 	},
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	success(...data: any[]): void {
+	success(...data: LogData): void {
 		console.log(`\x1b[32m%s\x1b[0m`, `[success] [${getFormattedDate()}]:`, ...data);
 	},
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	warn(...data: any[]): void {
+	warn(...data: LogData): void {
 		console.warn(`\x1b[33m%s\x1b[0m`, `[warning] [${getFormattedDate()}]:`, ...data);
 	},
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	error(...data: any[]): void {
+	error(...data: LogData): void {
 		console.error(`\x1b[31m%s\x1b[0m`, `[error] [${getFormattedDate()}]:`, ...data);
 	}
 };
