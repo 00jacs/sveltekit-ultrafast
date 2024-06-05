833f84f79dcc2c2bc2433ec27d8a295c2c8a66e8
jacobxcoder <gillbatesdev@gmail.com>
2024-06-05 22:13:29 +0200

    feat(utils): improve logger with proper coloring

--- src/lib/utils/logger.ts
+++ src/lib/utils/logger.ts
@@ -4,18 +4,29 @@ function getFormattedDate(): string {
 
 type LogData = unknown[];
 
+function colorize(color: string, ...data: LogData): string {
+	const colorCode = {
+		blue: '\x1b[36m',
+		green: '\x1b[32m',
+		yellow: '\x1b[33m',
+		red: '\x1b[31m',
+	}[color];
+
+	return `${colorCode}${data.join(' ')}\x1b[0m`;
+}
+
 export const logger = {
 	info(...data: LogData): void {
-		console.log(`\x1b[36m%s\x1b[0m`, `[info] [${getFormattedDate()}]: `, ...data);
+		console.log(colorize('blue', `[info] [${getFormattedDate()}]:`, ...data));
 	},
 	success(...data: LogData): void {
-		console.log(`\x1b[32m%s\x1b[0m`, `[success] [${getFormattedDate()}]:`, ...data);
+		console.log(colorize('green', `[success] [${getFormattedDate()}]:`, ...data));
 	},
 	warn(...data: LogData): void {
-		console.warn(`\x1b[33m%s\x1b[0m`, `[warning] [${getFormattedDate()}]:`, ...data);
+		console.warn(colorize('yellow', `[warning] [${getFormattedDate()}]:`, ...data));
 	},
 	error(...data: LogData): void {
-		console.error(`\x1b[31m%s\x1b[0m`, `[error] [${getFormattedDate()}]:`, ...data);
+		console.error(colorize('red', `[error] [${getFormattedDate()}]:`, ...data));
 	}
 };
 
